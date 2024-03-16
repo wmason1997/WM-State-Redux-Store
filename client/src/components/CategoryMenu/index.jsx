8260bb37a251865c3b93afb5714245db3c220061
wmason1997 <williamcmason1997@gmail.com>
2024-03-16 11:03:16 -0700

    Updated CategoryMenu component index.jsx.

--- client/src/components/CategoryMenu/index.jsx
+++ client/src/components/CategoryMenu/index.jsx
@@ -1,44 +1,31 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useQuery } from "@apollo/client";
-import StoreProvider from "../../utils/GlobalState";
-import {
-  UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY,
-} from "../../utils/actions";
-import { QUERY_CATEGORIES } from "../../utils/queries";
-import { idbPromise } from "../../utils/helpers";
+import { useDispatch, useSelector } from 'react-redux';
+import { updateCategories, updateCurrentCategory } from '../../utils/actions';
+import { QUERY_CATEGORIES } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
-  const [state, dispatch] = StoreProvider();
-
-  const { categories } = state;
+  const dispatch = useDispatch();
+  const categories = useSelector(state => state.categories);
 
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
     if (categoryData) {
-      dispatch({
-        type: UPDATE_CATEGORIES,
-        categories: categoryData.categories,
-      });
+      dispatch(updateCategories(categoryData.categories));
       categoryData.categories.forEach((category) => {
-        idbPromise("categories", "put", category);
+        idbPromise('categories', 'put', category);
       });
     } else if (!loading) {
-      idbPromise("categories", "get").then((categories) => {
-        dispatch({
-          type: UPDATE_CATEGORIES,
-          categories: categories,
-        });
+      idbPromise('categories', 'get').then((categories) => {
+        dispatch(updateCategories(categories));
       });
     }
   }, [categoryData, loading, dispatch]);
 
   const handleClick = (id) => {
-    dispatch({
-      type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: id,
-    });
+    dispatch(updateCurrentCategory(id));
   };
 
   return (
@@ -56,7 +43,7 @@ function CategoryMenu() {
       ))}
       <button
         onClick={() => {
-          handleClick("");
+          handleClick('');
         }}
       >
         All
